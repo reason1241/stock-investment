921a5215a6e4e1163cc099407b3b341b1c3cfa9e
reason1241 <reason1241@naver.com>
2025-10-02 11:11:09 +0900

    polish

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ def str_pct_to_float_pct(x):
     return x.str.replace("%","").astype(float)
     
 def parse_groupby_to_dict(x):
-    x.columns = [' '.join(col).strip() for col in x.columns.values]
+    x.columns = ['_'.join(col).strip() for col in x.columns.values]
     return x.to_dict(orient="index")
 
 def postprocess(d):
@@ -58,24 +58,33 @@ if __name__ == '__main__':
         'Perf Half', 'Perf Year', 'Volume', 'Price', 'Change']
         """
         
-        stock_data['Change Pct'] = str_pct_to_float_pct(stock_data['Change'])
-        stock_data['Perf Week Pct'] = str_pct_to_float_pct(stock_data['Perf Week'])
-        stock_data['Perf Month Pct'] = str_pct_to_float_pct(stock_data['Perf Month'])
+        # rename columns
+        stock_data.columns = [c.lower().replace(' ', '_') for c in stock_data.columns]
+                
+        stock_data['change_pct'] = str_pct_to_float_pct(stock_data['change'])
+        stock_data['perf_week_pct'] = str_pct_to_float_pct(stock_data['perf_week'])
+        stock_data['perf_month_pct'] = str_pct_to_float_pct(stock_data['perf_month'])
         
         # extract sector, industry
-        summary['Sector Agg'] = parse_groupby_to_dict(stock_data.groupby('Sector').agg({'Change Pct': ['min', 'max', *statistics]}))
-        summary['Industry Agg'] = parse_groupby_to_dict(stock_data.groupby('Industry').agg({'Change Pct': ['min', 'max', *statistics]}))        
+        summary['aggregates'] = {}
+        summary['aggregates']['by_sector'] = parse_groupby_to_dict(stock_data.groupby('sector').agg({'change_pct': ['min', 'max', *statistics]}))
+        summary['aggregates']['by_industry'] = parse_groupby_to_dict(stock_data.groupby('industry').agg({'change_pct': ['min', 'max', *statistics]}))        
         
         # extract min max change pct
-        columns = ['Ticker', 'Company', 'Sector', 'Industry', 'Market Cap', 'Change Pct', 'Perf Week Pct', 'Perf Month Pct']
+        columns = ['ticker', 'company', 'sector', 'industry', 'market_cap', 'change_pct', 'perf_week_pct', 'perf_month_pct']
         
         # extract weekly and monthly mean
-        summary['Change Pct Max Top3'] = stock_data.nlargest(3, 'Change Pct')[columns].to_dict(orient="records")
-        summary['Change Pct Min Top3'] = stock_data.nsmallest(3, 'Change Pct')[columns].to_dict(orient="records")
+        summary['change_pct'] = {}
+        summary['change_pct']['best3'] = stock_data.nlargest(3, 'change_pct')[columns].to_dict(orient="records")
+        summary['change_pct']['worst3'] = stock_data.nsmallest(3, 'change_pct')[columns].to_dict(orient="records")
 
-        # extract weekly and monthly mean
-        summary['Perf Week Pct mean'] = stock_data['Perf Week Pct'].mean()
-        summary['Perf Month Pct mean'] = stock_data['Perf Month Pct'].mean()
+        # extract summary
+        summary['summary'] = {}
+        summary['summary']['perf_week_pct_mean'] = stock_data['perf_week_pct'].mean()
+        summary['summary']['perf_month_pct_mean'] = stock_data['perf_month_pct'].mean()
+        summary['summary']['n_total'] = len(stock_data)
+        summary['summary']['n_change_pct_pos'] = len(stock_data[stock_data['change_pct'] >= 0])
+        summary['summary']['n_change_pct_neg'] = len(stock_data[stock_data['change_pct'] < 0])
         
         
         with open('./20251002/below_200sma_monthly_perf_summary.json', 'w') as f:
